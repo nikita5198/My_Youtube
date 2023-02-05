23c8d1cf4a0a4744327e49c9d47b1b3f59e2bd36
nikita5198 <nikitap5198@gmail.com>
2023-02-05 18:46:21 +0530

    working with api

--- src/context/contextApi.js
+++ src/context/contextApi.js
@@ -1,9 +1,31 @@
 import React,{createContext,useState,useEffect} from "react";
 import { fetchDataFromApi } from "../utils/api";
-export const context=createContext();
-const AppContext=(prop)=>{
+export const Context=createContext();
+
+
+export const AppContext=(props)=>{
    const [loading,setLoading]=useState(false);
    const [searchResult,setSearchResult]=useState(false);
-   const [selectCategories,setSelectCategories]=useState(false);
+   const [selectCategories,setSelectCategories]=useState("New");
    const [mobileMenu,setMobileMenu]=useState(false);
-}
+   useEffect(()=>{
+      fetchSelectedCategoryData(selectCategories);
+   },[]);
+     const fetchSelectedCategoryData=(query)=>{
+       
+     }
+  return (
+    <Context.Provider
+    value={{
+      loading,
+      setLoading,
+      searchResult,
+      setSelectCategories,
+      mobileMenu,
+      setMobileMenu,
+    }}
+    >
+     {props.childrem} 
+    </Context.Provider>
+  )
+   }
